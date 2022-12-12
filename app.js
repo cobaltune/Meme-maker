704524204521d475665e32690ddc6fb28c52e8a3
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-12 23:48:49 +0900

    ing

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+ctx.lineWidth = 2;
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+let isPainting = false;
 
-ctx.beginPath();
-ctx.fillStyle = 'white';
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting(event) {
+  isPainting = true;
+}
+
+function cancelPainting(event) {
+  isPainting = false;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+document.addEventListener('mouseleave', cancelPainting);
